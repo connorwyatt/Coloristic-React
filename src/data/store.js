71635c14113b7593fd601e58a850af74d7249406
Connor Wyatt <connorwyatt1@gmail.com>
2017-12-09 11:39:14 +0000

    Add redux observable

--- src/data/store.js
+++ src/data/store.js
@@ -1,12 +1,28 @@
 import type { Store as ReduxStore } from 'redux'
-import { createStore } from 'redux'
+import {
+  applyMiddleware,
+  compose,
+  createStore
+} from 'redux'
+import type { Action } from './actions'
+import { epics } from './epics'
 import type { State } from './reducers'
 import { reducers } from './reducers'
 
 export type Store = ReduxStore<State, Action>
 
+const middleware = createEpicMiddleware(epics)
+
+let appliedMiddleware = applyMiddleware(...middleware)
+
+if (process.env.NODE_ENV === 'development') {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+  appliedMiddleware = composeEnhancers(appliedMiddleware)
+}
+
 export const store: Store = createStore(
   reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()
+  appliedMiddleware
 )
--- src/types/window.js
+++ src/types/window.js
@@ -1,3 +1,9 @@
 // @flow
 
-declare var __REDUX_DEVTOOLS_EXTENSION__: ?Function
+declare var __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: ?Function
+
+declare var process: {
+  env: {
+    NODE_ENV: string
+  }
+}
